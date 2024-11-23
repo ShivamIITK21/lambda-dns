b784070d33f26dc7bca6d59b221557727d7cff98
Shivam <sshivam21@iitk.ac.in>
2024-11-24 02:00:05 +0530

    Added Qname reader function

--- src/Data/MonadicByteString.hs
+++ src/Data/MonadicByteString.hs
@@ -1,6 +1,7 @@
 module Data.MonadicByteString(readWords, read16bit) where
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 import Data.Word
 import Data.Bits
 import Control.Monad.State
@@ -9,7 +10,7 @@ import Control.Monad.State
 readWords::BS.ByteString -> Int -> State Int BS.ByteString 
 readWords bs n = do
                     offset <- get
-                    if offset + 1 >= BS.length bs then error "Reached the end"
+                    if offset + 1 >= BS.length bs then error "Nothing More to Read!"
                     else do 
                         let readBytes = BS.take n (BS.drop offset bs)
                         put (offset + BS.length readBytes)
@@ -27,5 +28,36 @@ read16bit bs = do
                     put (offset + 2)
                     return val
 
+readAt::BS.ByteString -> Int -> Int -> BS.ByteString
+readAt bs pos len = BS.take len (BS.drop pos bs)
 
-
+readQNameUtil::BS.ByteString -> Int -> Bool ->  State Int String
+readQNameUtil bs pos jumped = let byte = BS.index bs pos in 
+                                    if (byte .&. 0xC0) /= 0xC0 then
+                                        if byte == 0 then 
+                                            do 
+                                                put (pos + 1)
+                                                return ""
+                                        else
+                                            do
+                                                let chunkLen = fromIntegral byte
+                                                let chunk = BSC.unpack (readAt bs (pos + 1) chunkLen)
+                                                rest <- readQNameUtil bs (pos + 1 + chunkLen) jumped
+                                                if not (null rest) then return (chunk ++ ['.'] ++ rest)
+                                                else return (chunk ++ rest) 
+                                        
+                                    else
+                                        if not jumped then
+                                            do
+                                                put (pos + 2)
+                                                let secondByte = fromIntegral (BS.index bs (pos + 1)) :: Word16
+                                                let len = fromIntegral byte :: Word16
+                                                let offset = fromIntegral (((len .^. 0xC0) `shiftL` 8) .|. secondByte) :: Int
+                                                readQNameUtil bs offset True
+                                        else 
+                                            do
+                                                let secondByte = fromIntegral (BS.index bs (pos + 1)) :: Word16
+                                                let len = fromIntegral byte :: Word16
+                                                let offset = fromIntegral (((len .^. 0xC0) `shiftL` 8) .|. secondByte) :: Int
+                                                readQNameUtil bs offset True
+                                                
